8eb094895da496c2f407da2b38b51f8444120c90
祥严 <11263132@qq.com>
2020-10-27 10:02:48 +0800

    删除eclipse配置

--- aloli/src/main/java/com/aloli/config/InterceptorConfig.java
+++ aloli/src/main/java/com/aloli/config/InterceptorConfig.java
@@ -7,8 +7,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.aloli.interceptor.AuthenticationInterceptor;
 
+/**
+ * 定义一个配置类
+ */
 @Configuration
 public class InterceptorConfig implements WebMvcConfigurer {
+    /**
+     addInterceptor：需要一个实现HandlerInterceptor接口的拦截器实例
+     addPathPatterns：用于设置拦截器的过滤路径规则；addPathPatterns("/**")对所有请求都拦截
+     excludePathPatterns：用于设置不需要拦截的过滤规则
+     拦截器主要用途：进行用户登录状态的拦截，日志的拦截等。
+     */
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(authenticationInterceptor())
@@ -16,8 +25,12 @@ public class InterceptorConfig implements WebMvcConfigurer {
                 .excludePathPatterns("/static/**")
                 .excludePathPatterns("/error")
                 .excludePathPatterns("/login");
-
     }
+
+
+    /**
+     *将这个拦截器交给spring mvc 进行管理
+     */
     @Bean
     public AuthenticationInterceptor authenticationInterceptor() {
         return new AuthenticationInterceptor();
