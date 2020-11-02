32a7ef9e1146c586f8bf32f9f2ac35f2ecbb9df8
祥严 <11263132@qq.com>
2020-11-02 16:43:11 +0800

    删除eclipse配置

---
+++ aloli/src/main/java/com/aloli/controller/LoginController.java
@@ -0,0 +1,56 @@
+package com.aloli.controller;
+
+import java.util.List;
+
+
+import com.alibaba.fastjson.JSONObject;
+import com.aloli.annotation.UserLoginToken;
+import com.aloli.entity.User;
+import com.aloli.service.UserService;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import com.aloli.entity.Student;
+import com.aloli.mapper.StudentMapper;
+
+@RestController
+@RequestMapping("/login")
+public class LoginController {
+	@Autowired
+	UserService userService;
+
+	//登录
+	@PostMapping
+	public Object login(@RequestBody User user){
+		JSONObject jsonObject=new JSONObject();
+		User userForBase=userService.getOne(Wrappers.<User>lambdaQuery().eq(User::getUsername,user.getUsername()));
+		if(userForBase==null){
+			jsonObject.put("message","登录失败,用户不存在");
+			return jsonObject;
+		}else {
+			if (!userForBase.getPassword().equals(user.getPassword())){
+				jsonObject.put("message","登录失败,密码g'r错误");
+				return jsonObject;
+			}else {
+				String token = userService.getToken(userForBase);
+				jsonObject.put("token", token);
+				jsonObject.put("user", userForBase);
+				return jsonObject;
+			}
+		}
+	}
+
+	@GetMapping("/getMessage")
+	public String getMessage(){
+
+		return aa();
+	}
+	@UserLoginToken
+	private String  aa(){
+
+		return "aa";
+	}
+}
