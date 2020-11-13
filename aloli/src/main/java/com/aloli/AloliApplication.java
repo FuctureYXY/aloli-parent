5093cb60b881a4079be423b95fa4bcb1a57f43be
祥严 <11263132@qq.com>
2020-11-13 14:58:43 +0800

    代码基础建设

--- aloli/src/main/java/com/aloli/AloliApplication.java
+++ aloli/src/main/java/com/aloli/AloliApplication.java
@@ -5,9 +5,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.ConfigurableApplicationContext;
 //exclude = {DataSourceAutoConfiguration.class} 是为了使用  yml  配置 不设置设个  则DataSource报错
 @SpringBootApplication
+@EnableDiscoveryClient
 @MapperScan("com.aloli.mapper")//扫描 mapper
 @EnableCaching()//spring framework中的注解驱动的缓存管理功能
 public class AloliApplication {
@@ -15,7 +17,6 @@ public class AloliApplication {
 	public static void main(String[] args) {
 
 		ConfigurableApplicationContext run = SpringApplication.run(AloliApplication.class, args);
-
 		String property = run.getEnvironment().getProperty("user.age");
 		System.out.println(property);
 	}
